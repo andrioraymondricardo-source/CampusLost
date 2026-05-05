81d897c00e264266ac405fb7503083cbf93c6eae
sh0wmethem0ney <Hyeok.I.Kwon@student.uts.edu.au>
2026-05-05 18:59:55 +1000

    created initial project
    initial project files created

---
+++ CampusLost/CampusLost/CampusLostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusLostApp.swift
+//  CampusLost
+//
+//  Created by Hyeok In on 5/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct CampusLostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
