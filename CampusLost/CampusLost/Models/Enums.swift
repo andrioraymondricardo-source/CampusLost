72fa066852106f39822b9deecc410f5d20198850
sh0wmethem0ney <Hyeok.I.Kwon@student.uts.edu.au>
2026-05-05 19:40:24 +1000

    created data models (enums)
    University / ReportType / ItemCategory / ReportStatus added

--- CampusLost/CampusLost/Models/Enums.swift
+++ CampusLost/CampusLost/Models/Enums.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+//uni list selected by the user
+//raw string + storable (swift data) + uni.allcases available + for each usuable
 enum University: String, Codable, CaseIterable, Identifiable {
     case uts = "University of Technology Sydney"
     case usyd = "University of Sydney"
@@ -14,7 +16,37 @@ enum University: String, Codable, CaseIterable, Identifiable {
     case uow = "University of Western Australia"
     case other = "Other"
     
+    //use raw string value as an ID
     var id: String { rawValue }
 }
 
+//report for a lost or found?
+enum ReportType: String, Codable, CaseIterable, Identifiable {
+    case lost = "Lost"
+    case found = "Found"
+    
+    var id: String { rawValue }
+}
 
+//item categories selected for reports
+//for easier filtering and searching
+enum ItemCategory: String, Codable, CaseIterable, Identifiable {
+    case electronics = "Electronics"
+    case cards = "Cards"
+    case clothing = "Clothing"
+    case books = "Books"
+    case keys = "Keys"
+    case bags = "Bags"
+    case other = "Other"
+    
+    var id: String {rawValue}
+}
+
+//current state of report
+//active = visible & resolved = hidden (maybe?)
+enum ReportStatus: String, Codable, CaseIterable, Identifiable {
+    case active = "Active"
+    case resolved = "Resolved"
+    
+    var id: String {rawValue}
+}
